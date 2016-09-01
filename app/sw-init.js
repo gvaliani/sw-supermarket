84d8db5ff28bb4df3539604465262cf6c2b31782
Gerardo Valiani <gerardovaliani@gmail.com>
2016-09-01 16:05:20 -0300

    Removed useless code.

--- app/sw-init.js
+++ app/sw-init.js
@@ -1,32 +1,11 @@
-if ('serviceWorker' in navigator) {
-			navigator.serviceWorker.register('./sw.js', { scope: '/' })
-			.then(function(registration) {
-				console.log("Service Worker Registered");
-        console.log('registration: ', registration )
-        // return registration.pushManager.getSubscription()
-        //   .then(function(subscription) {
-        //     if (subscription) {
-        //       return subscription;
-        //     }
-
-        //     return registration.pushManager.subscribe({ userVisibleOnly: true });
-        //   });
-      })
-      .catch(function(err) {
-        console.log("Service Worker Failed to Register", err);
-      })
-
-      console.log( 'El controlador es: ', navigator.serviceWorker.controller);
-
-      navigator.serviceWorker.oncontrollerchange = function() {
-        this.controller.onstatechange = function() {
-          console.log('Controller has changed: ', this);
-
-          navigator.serviceWorker.controller.postMessage('totto');
-        };
-      }
-
-      navigator.serviceWorker.ready.then(function(swRegistration) {
-        // return swRegistration.sync.register('myFirstSync');
-      });
-    }
+if ( 'serviceWorker' in navigator ) {
+  navigator.serviceWorker.register( './sw.js', { scope: '/' } )
+  .then( function( registration ) {
+    // eslint-disable-next-line no-console
+    console.log( 'Service Worker has been registered: ', registration );
+  } )
+  .catch( function( err ) {
+    // eslint-disable-next-line no-console
+    console.log( 'Service Worker Failed to Register', err );
+  } )
+}
