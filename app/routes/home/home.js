5a02049b0e13392764d8a34c6923e685e7f6dcab
Gerardo Valiani <gerardovaliani@gmail.com>
2016-09-07 12:41:31 -0300

    Implemented saving status in toggle cart. Improved quality code.

--- app/routes/home/home.js
+++ app/routes/home/home.js
@@ -13,24 +13,22 @@ function homeCtrl( productService ) {
   } );
 
   function add_product() {
+    let clone_product;
+
     Object.assign( self.product, {
       tempId: Date.now(),
       inCart: false
     } );
 
-    let temp_product = Object.assign( {}, self.product );
-    temp_product.status = 'Pending...';
-    self.products.push( temp_product );
+    clone_product = Object.assign( {}, self.product );
+    clone_product.status = 'Pending...';
+    self.products.push( clone_product );
 
     productService
       .post( self.product )
       .then( function successPost( new_product ) {
 
-        self.products.map( function find_product( item, key, arr ) {
-          if ( item.tempId == new_product.tempId ) {
-            arr[ key ] = new_product;
-          }
-        } );
+        replace_item( clone_product, new_product );
 
       } );
 
@@ -38,9 +36,21 @@ function homeCtrl( productService ) {
   }
 
   function toggle_cart( product ) {
+    let clone_product;
+
     product.inCart = !product.inCart;
+
+    clone_product = Object.assign( {
+      status: 'Pending...'
+    }, product );
+
+    replace_item( product, clone_product );
+
     productService
-      .put( product );
+      .put( product )
+      .then( function after_saved( saved_product ) {
+        replace_item( clone_product, saved_product );
+      } );
   }
 
   function reset_list() {
@@ -87,6 +97,38 @@ function homeCtrl( productService ) {
       .then( function setProductList( res ) {
         self.products = res;
       } );
+  }
+
+  // function find_item( item_wanted ) {
+  //   let result,
+  //     item_wanted_id;
+
+  //   item_wanted_id = item_wanted.id || item_wanted.tempId;
+
+  //   result = self.products.filter( function filter_by_id( item ) {
+  //     let item_id = item.id || item.tempId;
+  //     if ( item_wanted_id === item_id ) {
+  //       return true;
+  //     }
+
+  //     return false;
+  //   } ).shift();
+
+  //   return result;
+  // }
+
+  function replace_item( original_item, new_item ) {
+    let original_item_id;
+
+    original_item_id = original_item.id || original_item.tempId;
+
+    self.products.map( function find_product( item, key, arr ) {
+      let item_id = item.id || item.tempId;
+
+      if ( original_item_id == item_id ) {
+        arr[ key ] = new_item;
+      }
+    } );
   }
 }
 
