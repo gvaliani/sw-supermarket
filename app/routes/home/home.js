556284716dec457cb85e194410afae809e1a7c8c
Gerardo Valiani <gerardovaliani@gmail.com>
2016-09-07 10:41:38 -0300

    Removed useless code.

--- app/routes/home/home.js
+++ app/routes/home/home.js
@@ -1,7 +1,7 @@
-homeCtrl.$inject = [ '$scope', 'productService' ];
-function homeCtrl( $scope, productService ) {
+homeCtrl.$inject = [ 'productService' ];
+function homeCtrl( productService ) {
   let self = this;
-  console.log('$scope: ', $scope)
+
   init();
 
   return Object.assign( self, {
@@ -88,52 +88,6 @@ function homeCtrl( $scope, productService ) {
         self.products = res;
       } );
   }
-  // function add_price_to_product( id, price ) {
-  //   console.log("me llamaron!");
-  //   for ( var product in $rootScope.product_list ) {
-  //     if ( $rootScope.product_list[product].id == id ) {
-  //       $rootScope.product_list[product].price = price;
-  //       self.product.newPrice = '';
-  //       $('#product_'+id).click();
-  //       break;
-  //     }
-  //   }
-  //   self.product = '';
-  // }
-
-  // function buy_product(product, $index){
-  //   $('#product_'+product.id).click();
-  //   $rootScope.bought_product_list.push(product);
-  //   $rootScope.product_list.splice($index, 1);
-  // }
-
-  // function remove_product($index){
-  //   $('#product_'+$index).click();
-  //   $rootScope.product_list.splice($index, 1);
-  // }
-
-  // function init(){
-  //   <script type="text/javascript" src="app/routes/bought/bought.js"></script>
-  //   self.add_product = add_product;
-  //   self.add_price_to_product = add_price_to_product;
-  //   self.buy_product = buy_product;
-  //   self.remove_product = remove_product;
-  //   self.product = '';
-  //   self.id = 0;
-  //   self.product = {
-  //     id:'',
-  //     name:'',
-  //     price:''
-  //   };
-  //   console.log($rootScope.product_list);
-  //   if($rootScope.product_list === undefined){
-  //     $rootScope.product_list = [];
-  //   }
-  //   if($rootScope.bought_product_list === undefined){
-  //     $rootScope.bought_product_list = [];
-  //   }
-  // }
-  // init();
 }
 
 module.exports = homeCtrl;
